037ad1ca90c24c66b7bd6b0e1253a57875bff4f9
meaydino <hotknow-34@outlook.com>
2026-01-09 15:58:50 +0300

    fixed:ex04 hpp

--- cpp01/ex04/FileReplacer.hpp
+++ cpp01/ex04/FileReplacer.hpp
@@ -9,17 +9,15 @@ private:
     std::string _filename;
     std::string _s1;
     std::string _s2;
+    std::string _readFile();
+    bool _writeFile(const std::string& content);
+    std::string _replaceString(const std::string& content);
 
 public:
     FileReplacer(std::string filename, std::string s1, std::string s2);
     ~FileReplacer();
     
     bool replaceInFile();
-    
-private:
-    std::string _readFile();
-    bool _writeFile(const std::string& content);
-    std::string _replaceString(const std::string& content);
 };
 
 #endif
